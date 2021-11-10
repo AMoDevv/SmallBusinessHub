984e075e5c2b053d7157da305f4099c9d31faee7
Alain <alainmorrisdev@gmail.com>
2021-11-09 23:34:37 -0600

    adding routing and pages for editing profiles

---
+++ editGeneralUser.php
@@ -0,0 +1,37 @@
+<?php
+//Initalize session
+session_start();
+// Include the database configuration file  
+require_once 'config.php';
+require_once "navigation.php";
+
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Edit General User</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.ico" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+</head>
+
+<body>
+    <!--Query Database and get user information to display in the fields.
+        For the photo, display the photo in a nice size and add a button to change profile photo.
+        Rest of data can just be filled out in fields ready to be edited.
+    -->
+</body>
+
+
+</html>
