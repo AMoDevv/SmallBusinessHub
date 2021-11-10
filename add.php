a904873c75e5adec398b65b6e72deff7ad6e2564
Alain <alainmorrisdev@gmail.com>
2021-11-09 22:16:19 -0600

    add post fixed

--- add.php
+++ add.php
@@ -16,18 +16,34 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Home</title>
+    <title>Add Post</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
     <link rel="stylesheet" href="css/add-post.css">
+    <style>
+        #uploadBtn{
+    border: 3px solid #555;
+    color: white;
+    background: #666;
+    margin: 10px 0;
+    border-radius: 5px;
+    font-weight: bold;
+    padding: 5px 20px;
+    cursor: pointer;
+  }
+    </style>
 </head>
 
 <body>
-    <form action="phpscripts/uploadPost.php" method="post" enctype="multipart/form-data">
+    <div class="alert alert-danger" role="alert" style="display: none; margin-bottom: 0px;" id="generalAlert">
+            Please ensure that all fields are filled out and a picture included to post.
+        </div>
+    <form action="phpscripts/uploadPost.php" method="post" enctype="multipart/form-data" id="addPostForm">
         <div class="form-group post-body">
             <center><h2>Create a new post</h2></center>
             <br>
@@ -36,12 +52,12 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         <br>
             <div class="upload-wrapper">
             <span class="file-name" id="fileName">Choose a file...</span>
-            <label for="file-upload">Browse<input type="file" id="file-upload" name="uploadedFile" required onchange='updateFileName(this)'></label>
+            <label for="file-upload">Browse<input type="file" accept = ".png,.jpg,.jpeg" id="file-upload" name="uploadedFile" required onchange='updateFileName(this)'></label>
                 </div>
         <br>
         <label for="tags">Tags eg. #cooking #food #seafood #gourmet</label>
         <textarea class="form-control" id="tags" name="tags" rows="1" cols="50" required></textarea>
-        <center><input type="submit" name="uploadBtn" value="Upload" /></center>
+        <center><input name="uploadBtn" type="button" value="Upload" id='uploadBtn'/></center>
         <br>
         </div>
     </form>
@@ -56,5 +72,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         fileName.textContent = name;
     }
 </script>
+
+<script src="js/add_post.js"></script>
 
 </html>
