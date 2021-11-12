ca8888d7e31ef7c8fd832f6a7db299101df43132
Bidair Gonzalez <bidair13@gmail.com>
2021-11-11 23:00:36 -0600

    Added top 4 trending and top products by category

--- home.php
+++ home.php
@@ -1,16 +1,84 @@
 <?php
 //Initalize session
 session_start();
+use App\Models\BusinessInformation;
+use App\Models\Category;
+use App\Models\Tags;
+
 // Include the database configuration file  
-require_once 'config.php';
-//require_once "navigation.php";
+require_once './config.php';
+require_once "./navigation.php";
+
+require_once "./models/tags-model.php";
+require_once "./models/category-model.php";
+require_once "./models/result.php";
+require_once "./models/business-model.php";
+
 
 // Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
+
+// $search_term = $_GET["q"];
+$search_term = strtolower("fish");
+// if( no $search ){
+//     // Redirect
+// }
+$out = array();
+$tag_rat = 0.7;
+$cat_rat = 0.7;
+$bus_rat = 0.7;
+
+// TAGS
+$tag_search = new Tags();
+$tag_result = $tag_search->getUniqueTags($mysqli);
+// Sort by tag match
+foreach ($tag_result as $tag) {
+    // output data of each row
+    $tag_name = strtolower($tag->tag);
+    $rat = 1 - levenshtein($tag_name, $search_term) / max(strlen($search_term), strlen($tag_name));
+    if($rat > $tag_rat) {
+        array_push($out, new Result($tag->tag, $rat, "Tag", $tag->tag, $tag->photo));
+    }
+}
+
+
+// Categories
+$cat_search = new Category();
+$cat_result = $cat_search->getUniqueCategory($mysqli);
+// Sort by tag match
+foreach ($cat_result as $cat) {
+    // output data of each row
+    $tag = strtolower($cat);
+    $rat = 1 - levenshtein($tag, $search_term) / max(strlen($search_term), strlen($tag));
+    if($rat > $cat_rat) {
+        array_push($out, new Result($cat->name, $rat, "Category", $cat->id, $cat->photo));
+    }
+}
+
+
+// Business
+$business_search = new BusinessInformation();
+$business_result = $business_search->getBusinessNames($mysqli);
+// Sort by tag match
+foreach ($business_result as $business) {
+    // output data of each row
+
+    $tag = strtolower($business->business_name);
+    $rat = 1 - levenshtein($tag, $search_term) / max(strlen($search_term), strlen($tag));
+    if($rat > $bus_rat) {
+        array_push($out, new Result($business->business_name, $rat, "Business", $business->business_id, $business->image));
+    }
+}
+
+usort($out, function ($a,$b) {
+    return $a->getVal()<$b->getVal();
+});
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -172,6 +240,50 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     </div>
   </div>
 </div>
+<!-- =============Trending================ -->
+<?php
+
+if ($result = $mysqli -> query("
+SELECT p.post_id
+FROM 
+    (
+        SELECT post_id, COUNT(*) as c
+        FROM saves
+        WHERE DATEDIFF(CURRENT_TIMESTAMP, DateCreated) < 7
+        GROUP BY post_id
+        ORDER BY c DESC
+    )
+as p
+LIMIT 4; ")) 
+{
+  echo "Returned rows are: " . $result -> num_rows;
+  // Free result set
+  
+}
+
+?>
+
+<!-- ==========Top food=============== -->
+  
+<div style="width: 100%; padding: 40px;">
+            <h1>Results</h1>
+            <div style='width: 100%;'>
+            <?php
+                foreach($out as $output){
+                    $image = "data:image/jpg;base64,".base64_encode($output->photo);
+                    echo "
+                    <div style='width: 33%; float: left;'>
+                    <h1>$output->name</h1>
+                    <img src='$image' class='post-images'>
+                    <p>This is a $output->type</p>
+                    <a href='/SmallBusinessHub/$output->type/$output->id'>'/SmallBusinessHub/$output->type/$output->id'</a>
+                    </div>
+                    ";
+                }
+            ?>
+            </div>
+  </div>
+
 
 <footer class="p-10 bg-gradient-to-r from-green-400 to-blue-500 ">
   <p> Footer</p>
