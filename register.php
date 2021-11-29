883da1ad6036c8aa4adad17bd1d7945d96ce2707
pablocawichii <pablocawichii@gmail.com>
2021-11-28 19:13:23 -0600

    Change password form 6 to 8 characters

--- register.php
+++ register.php
@@ -44,8 +44,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate password
     if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
-    } elseif (strlen(trim($_POST["password"])) < 6) {
-        $password_err = "Password must have atleast 6 characters.";
+    } elseif (strlen(trim($_POST["password"])) < 8) {
+        $password_err = "Password must have atleast 8 characters.";
     } else {
         $password = trim($_POST["password"]);
     }
